394035936159345f0ef32ef6b69eb8cbb720f3a5
Charlebois Christophe <dianketohead@gmail.com>
2022-07-27 17:34:57 +0200

    feat: add react-flow example on main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Sidebar from "../components/Sidebar";
+import OverviewFlow from "../components/Example";
 import "bulma/css/bulma.min.css";
 
 const Home = () => {
   return (
     <React.Fragment>
-      <section className="main-content columns is-fullheight">
+      <section className="main-content columns">
         <Sidebar />
-
         <div className="container column is-10">
-          <p>React flow</p>
+          <OverviewFlow />
         </div>
       </section>
     </React.Fragment>
