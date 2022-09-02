e08d9a090d5df6415275a3d88c8c5f7f847cca46
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-09-03 00:02:05 +0200

    feat: Add style project preview

--- src/components/PreviewProject/index.tsx
+++ src/components/PreviewProject/index.tsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom";
+import "./index.scss";
 
 const PreviewProject = (props: { data: any }) => {
   return (
@@ -8,18 +9,19 @@ const PreviewProject = (props: { data: any }) => {
       state={{ infoProject: props.data }}
       className="previewProject card p-3">
       <div className="card-content">
-        <p className="title is-size-6 mb-1">Title</p>
+        <p className="title is-size-6 mb-1">{ props.data.name }</p>
         <p className="subtitle">Subtitle</p>
         <div className="content mb-1 is-flex is-justify-content-space-between is-align-items-center">
           <p>Created</p>
           <p className="tag is-success is-light">01 october 2022</p>
         </div>
-      </div>
-      <div className="card-footer-item">
-        <div className="is-flex is-justify-content-space-between is-align-items-center">
-          <span className="image is-32x32 is-cover"></span>
-          <p className="button is-primary ml-auto">Open</p>
+        <div className="content mb-1 is-flex is-justify-content-space-between is-align-items-center">
+          <p>Techno</p>
+          <p className="tag is-warning is-light">xxxx</p>
         </div>
+      </div>
+      <div className="is-flex is-justify-content-space-between">
+        <p className="button is-primary ml-auto">Open</p>
       </div>
     </Link>
   );
