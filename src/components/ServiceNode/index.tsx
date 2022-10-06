34afb2c859693c5aa91a0bf3dc9d698045e80db7
Dimitri Vildina <Dimitri.vildina@gmail.com>
2022-10-06 12:19:47 +0200

    feat: dyn service node

--- src/components/ServiceNode/index.tsx
+++ src/components/ServiceNode/index.tsx
@@ -3,7 +3,7 @@ import { Handle, Position } from "reactflow";
 import "bulma/css/bulma.min.css";
 
 export const ServiceNode = ({ data }: any) => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   console.log(data)
 
@@ -12,10 +12,10 @@ export const ServiceNode = ({ data }: any) => {
       <Handle type="target" position={Position.Top} />
       {isOpen ? (
         <div className="text-updater-node">
-          <p>ami: ami-085925f297f89fce1</p>
-          <p>instance_type: t2.micro</p>
-          <p>ports: [8080]</p>
-          <p>count: 1</p>
+          <p>Service : {data.label}</p>
+          {data.extras && data.extras.map((item: any) => (
+            <p key={item.name}>{item.name} : </p>
+          ))}
           <button
             onClick={() => setIsOpen(false)}
             className="js-modal-trigger"
@@ -27,21 +27,74 @@ export const ServiceNode = ({ data }: any) => {
       ) : (
         <div className="text-updater-node">
           <h1>Edit Service</h1>
-          <p>ami:</p>
-          <input
-            className="input"
-            type="text"
-            placeholder="ami-085925f297f89fce1"
-          />
-          <p>instance_type:</p>
-          <input className="input" type="text" placeholder="t2.micro" />
-
-          <p>ports:</p>
-          <input className="input" type="text" placeholder="8080" />
-
-          <p>count:</p>
-          <input className="input" type="text" placeholder="1" />
-
+          {(() => {
+            return data.extras.map((item: any) => {
+              switch (true) {
+                case (item.type === "string" && item.choices == null): return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    {item.choices}
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="ami-085925f297f89fce1"
+                    />
+                  </fieldset>
+                )
+                case (item.type === "string" && item.choices != null): return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    <select>
+                      {item.choices.map((option: any) => (
+                        <option key={option} value={option}>{option}</option>
+                      ))}
+                    </select>
+                  </fieldset>
+                )
+                case (item.type === "array" && item.choices == null && item.is_multiple_choice == false): return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="ami-085925f297f89fce1"
+                    />
+                    <p>please separate with commas</p>
+                  </fieldset>
+                )
+                case (item.type === "array" && item.choices != null && item.is_multiple_choice == true): return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    <select multiple>
+                      {item.choices.map((option: any) => (
+                        <option key={option} value={option}>{option}</option>
+                      ))}
+                    </select>
+                  </fieldset>
+                )
+                case item.type === "integer": return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    <input
+                      className="input"
+                      type="number"
+                      placeholder="ami-085925f297f89fce1"
+                    />
+                  </fieldset>
+                )
+                case item.type === "boolean": return (
+                  <fieldset key={item.name}>
+                    <label>{item.name}</label>
+                    <select>
+                      <option value='yes'>yes</option>
+                      <option value='no'>no</option>
+                    </select>
+                  </fieldset>
+                )
+                default: return
+              }
+            })
+          })()}
           <button onClick={() => setIsOpen(true)}>Edit</button>
         </div>
       )}
